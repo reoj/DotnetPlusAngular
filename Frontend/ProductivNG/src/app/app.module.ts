411982e00fe53d8345883d55a95e7d692a3b6b5b
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2024-07-09 14:46:51 -0600

    Replaces example CSS with Angular Material

--- Frontend/ProductivNG/src/app/app.module.ts
+++ Frontend/ProductivNG/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GoalsComponent } from './Components/goals/goal.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
