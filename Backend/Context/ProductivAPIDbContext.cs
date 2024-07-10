3718d17817e9d8876cfd8385775ea3cb0529ffe6
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2024-07-10 12:26:08 -0600

    Implements DB context

--- Backend/Context/ProductivAPIDbContext.cs
+++ Backend/Context/ProductivAPIDbContext.cs
@@ -6,45 +6,57 @@ public class ProductivAPIDbContext : DbContext
     public ProductivAPIDbContext(DbContextOptions<ProductivAPIDbContext> options) : base(options)
     { }
 
+    public DbSet<Tag> Tags { get; set; }
     public DbSet<Goal> Goals { get; set; }
     public DbSet<GoalTask> GoalTasks { get; set; }
-    public DbSet<Tag> Tags { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        SetUpGoalTaskRelationship(modelBuilder);
-        SetupTagGoalsRelationship(modelBuilder);
         SeedGoalsData(modelBuilder);
+        SetupTagGoalsRelationship(modelBuilder);
+        SetUpGoalTaskRelationship(modelBuilder);
     }
 
     private static void SeedGoalsData(ModelBuilder modelBuilder)
     {
-        Tag programmingTag = new Tag { Id = Guid.NewGuid(), Name = "Programming" };
-        GoalTask taskItem = new GoalTask
-        {
-            Id = Guid.NewGuid(),
-            Name = "Learn C# Basics",
-            Description = "Learn the basics of C#",
-            CreatedDate = DateTime.Now,
-            DueDate = DateTime.Now.AddDays(10),
-            IsComplete = false,
-        };
+        var FirstGoalId = Guid.NewGuid();
+        var FirstGoalTaskId = Guid.NewGuid();
+        var FirstTagId = Guid.NewGuid();
+
         Goal goalData = new Goal
         {
-            Id = Guid.NewGuid(),
+            Id = FirstGoalId,
             Name = "Learn C#",
             Description = "Learn C# to build web applications",
             CreatedDate = DateTime.Now,
             DueDate = DateTime.Now.AddDays(30),
             IsComplete = false,
             ProgressPercentage = 0,
-            GoalTasks = new List<GoalTask> { taskItem },
         };
-        modelBuilder.Entity<Goal>().HasData(goalData);
-        modelBuilder.Entity<GoalTask>().HasData(taskItem);
+        Tag programmingTag = new Tag
+        {
+            Id = FirstTagId,
+            Name = "Programming",
+            Goals = new List<Goal> { goalData }
+        };
+
+        GoalTask taskItem = new GoalTask
+        {
+            Id = FirstGoalTaskId,
+            Name = "Learn C# Basics",
+            Description = "Learn the basics of C#",
+            CreatedDate = DateTime.Now,
+            DueDate = DateTime.Now.AddDays(10),
+            IsComplete = false,
+            GoalId = FirstGoalId,
+            Goal = goalData
+        };
+
         modelBuilder.Entity<Tag>().HasData(programmingTag);
+        modelBuilder.Entity<GoalTask>().HasData(taskItem);
+        modelBuilder.Entity<Goal>().HasData(goalData);
     }
 
     private static void SetUpGoalTaskRelationship(ModelBuilder modelBuilder)
--- Backend/Models/GoalTask.cs
+++ Backend/Models/GoalTask.cs
@@ -1,13 +1,18 @@
 using Backend.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class GoalTask
 {
+    [Key]
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public DateTime CreatedDate { get; set; }
     public DateTime DueDate { get; set; }
     public bool IsComplete { get; set; }
+
+    [ForeignKey("Goal")]
     public Guid GoalId { get; set; }
     public Goal Goal { get; set; } = null!;
 }
