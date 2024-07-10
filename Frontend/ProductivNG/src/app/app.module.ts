f0dd833b847dd9047f3ecbe7808122e6cd4b286f
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2024-07-10 12:18:20 -0600

    Updates modules to work with Angular Material

--- Frontend/ProductivNG/src/app/app.module.ts
+++ Frontend/ProductivNG/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GoalsComponent } from './Components/goals/goal.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GoalsDisplayComponent } from './Components/goals-display/goals-display.component';
+import { AppCommonsModule } from './Modules/app-commons/app-commons.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { GoalsDisplayComponent } from './Components/goals-display/goals-display.
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppCommonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
